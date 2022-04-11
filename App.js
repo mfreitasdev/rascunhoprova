f9124bc0f6227bdf60ec07f2d7ac0e4219dcd1a7
mfreitasdev <matheus_mt.2011@hotmail.com>
2022-04-10 23:29:26 -0300

    calculo ok no alert

--- App.js
+++ App.js
@@ -1,24 +1,36 @@
 import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TouchableHighlight, TextInput } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableHighlight, TouchableOpacity ,TextInput } from 'react-native';
 
 export default function App() {
 
-const [nota1, setNota1]=useState(0);
-const [nota2, setNota2]=useState(0);
-const [nota3, setNota3]=useState(0);
+const [nota1, setNota1]=useState('');
+const [nota2, setNota2]=useState('');
+const [nota3, setNota3]=useState('');
 const [resultado, setResultado]=useState(0);
 
-const acao=()=>{
+/*const acao=()=>{
   return setResultado(((parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) /3 ).toFixed(2))
 }
+*/
+function acaodobotao(){
+  const calcular = (((parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) /3 ).toFixed(2));
+  
+  if(calcular >= 7 ){
+    alert('Você está aprovado!');
+  } else if(calcular >= 5 && calcular < 7){
+    alert('Você está na prova final!');
+  } else if(calcular < 5){
+    alert('Você está reprovado!');
+  }
+
+}
+
+
 
 
   return (
     <SafeAreaView style={estilos.conteiner}>
-        <View style={estilos.header}>
-
-        </View>
 
 
 
@@ -33,7 +45,8 @@ const acao=()=>{
         <TextInput
           style={estilos.display}
           value={nota1}
-          onChangeText={setNota1}
+          onChangeText={(nota1) => setNota1(nota1)}
+          placeholder="Digite a primeira nota"
           keyboardType='numeric'
 
         ></TextInput>
@@ -41,7 +54,8 @@ const acao=()=>{
         <TextInput
           style={estilos.display}
           value={nota2}
-          onChangeText={setNota2}
+          onChangeText={(nota2) => setNota2(nota2)}
+          placeholder="Digite a segunda nota"
           keyboardType='numeric'
 
         ></TextInput>
@@ -49,24 +63,18 @@ const acao=()=>{
         <TextInput
           style={estilos.display}
           value={nota3}
-          onChangeText={setNota3}
+          onChangeText={(nota3) => setNota3(nota3)}
+          placeholder="Digite a terceira nota"
           keyboardType='numeric'
 
         ></TextInput>
 
-        <TextInput
-          style={estilos.display}
-          value={resultado}
-          onChangeText={setResultado}
-          keyboardType='numeric'
-
-        ></TextInput>
-
-        <TouchableHighlight style={estilos.btn}
-          onPress={() => acao()}
+        <TouchableOpacity 
+          style={estilos.button}
+          onPress={acaodobotao}
         >
-          <Text>Calcular</Text>
-        </TouchableHighlight>
+          <Text style={estilos.buttonText}>Calcular</Text>
+        </TouchableOpacity>
         </View>
       </SafeAreaView>
   );
@@ -81,11 +89,17 @@ const estilos = StyleSheet.create({
     borderRadius:10,
     padding:10,
   },
-  btn:{
+  button:{
     backgroundColor:'#aaa',
-    padding:20,
+    padding:15,
+    justifyContent: 'center',
+    alignItems:'center',
+    margin: 15,
+    
   },
-  txtBtn:{
+  buttonText:{
+    color:'#FFF',
+    fontSize:25,
 
   },
   header:{
